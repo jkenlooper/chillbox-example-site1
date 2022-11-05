f720e8b83c6e38bbb6e32e17e967d5fcd6d29752
Jake Hickenlooper <jake@weboftomorrow.com>
2022-11-05 10:15:25 -0600

    Cleanup

--- bin/local-start.sh
+++ bin/local-start.sh
@@ -41,81 +41,98 @@ site_version_string="$(make --silent -C "$project_dir" inspect.VERSION)"
 immutable_example_hash="$(make --silent -C "$project_dir/immutable-example/" inspect.HASH)"
 
 # The ports on these do not need to be exposed since nginx is in front of them.
-docker image rm "$slugname-immutable-example" > /dev/null 2>&1 || printf ""
-DOCKER_BUILDKIT=1 docker build \
-    --target build \
-    -t "$slugname-immutable-example" \
-    "${project_dir}/immutable-example"
-docker run -d \
-  --network chillboxnet \
-  --mount "type=bind,src=${project_dir}/immutable-example/src,dst=/build/src" \
-  --name "$slugname-immutable-example" "$slugname-immutable-example"
 
-docker image rm "$slugname-api" > /dev/null 2>&1 || printf ""
-DOCKER_BUILDKIT=1 docker build \
-  -t "$slugname-api" \
-  "${project_dir}/api"
-# Switch to root user when troubleshooting or using bind mounts
-echo "Running the $slugname-api container with root user."
-docker run -d --tty \
-  --name "$slugname-api" \
-  --user root \
-  -e SERVER_NAME="site1-api:8100" \
-  --network chillboxnet \
-  --mount "type=bind,src=${project_dir}/api/src/site1_api,dst=/usr/local/src/app/src/site1_api,readonly" \
-  $slugname-api ./flask-run.sh
+build_start_immutable_example() {
+  docker image rm "$slugname-immutable-example" > /dev/null 2>&1 || printf ""
+  DOCKER_BUILDKIT=1 docker build \
+      --target build \
+      -t "$slugname-immutable-example" \
+      "${project_dir}/immutable-example"
+  docker run -d \
+    --network chillboxnet \
+    --mount "type=bind,src=${project_dir}/immutable-example/src,dst=/build/src" \
+    --name "$slugname-immutable-example" "$slugname-immutable-example"
+}
 
-docker image rm "$slugname-chill-static-example" > /dev/null 2>&1 || printf ""
-DOCKER_BUILDKIT=1 docker build \
-    -t "$slugname-chill-static-example" \
-    "${project_dir}/chill-static-example"
-docker run -d \
-  --name "$slugname-chill-static-example" \
-  --network chillboxnet \
-  --mount "type=bind,src=${project_dir}/chill-static-example/documents,dst=/home/chill/app/documents" \
-  --mount "type=bind,src=${project_dir}/chill-static-example/queries,dst=/home/chill/app/queries" \
-  --mount "type=bind,src=${project_dir}/chill-static-example/templates,dst=/home/chill/app/templates" \
-  -e CHILL_MEDIA_PATH="/media/" \
-  -e CHILL_THEME_STATIC_PATH="/theme/0/" \
-  -e CHILL_DESIGN_TOKENS_HOST="/design-tokens/0/" \
-  -e IMMUTABLE_EXAMPLE_URI="/immutable-example/v1/fake-hash/" \
-  "$slugname-chill-static-example"
+build_start_api() {
+  docker image rm "$slugname-api" > /dev/null 2>&1 || printf ""
+  DOCKER_BUILDKIT=1 docker build \
+    -t "$slugname-api" \
+    "${project_dir}/api"
+  # Switch to root user when troubleshooting or using bind mounts
+  echo "Running the $slugname-api container with root user."
+  docker run -d --tty \
+    --name "$slugname-api" \
+    --user root \
+    -e SERVER_NAME="site1-api:8100" \
+    --network chillboxnet \
+    --mount "type=bind,src=${project_dir}/api/src/site1_api,dst=/usr/local/src/app/src/site1_api,readonly" \
+    $slugname-api ./flask-run.sh
+}
 
-docker image rm "$slugname-chill-dynamic-example" > /dev/null 2>&1 || printf ""
-DOCKER_BUILDKIT=1 docker build \
-    -t "$slugname-chill-dynamic-example" \
-    "${project_dir}/chill-dynamic-example"
-docker run -d \
-  --name "$slugname-chill-dynamic-example" \
-  --network chillboxnet \
-  --mount "type=bind,src=${project_dir}/chill-dynamic-example/documents,dst=/home/chill/app/documents" \
-  --mount "type=bind,src=${project_dir}/chill-dynamic-example/queries,dst=/home/chill/app/queries" \
-  --mount "type=bind,src=${project_dir}/chill-dynamic-example/templates,dst=/home/chill/app/templates" \
-  -e CHILL_MEDIA_PATH="/media/" \
-  -e CHILL_THEME_STATIC_PATH="/theme/0/" \
-  -e CHILL_DESIGN_TOKENS_HOST="/design-tokens/0/" \
-  "$slugname-chill-dynamic-example"
+build_start_chill_static_example() {
+  docker image rm "$slugname-chill-static-example" > /dev/null 2>&1 || printf ""
+  DOCKER_BUILDKIT=1 docker build \
+      -t "$slugname-chill-static-example" \
+      "${project_dir}/chill-static-example"
+  docker run -d \
+    --name "$slugname-chill-static-example" \
+    --network chillboxnet \
+    --mount "type=bind,src=${project_dir}/chill-static-example/documents,dst=/home/chill/app/documents" \
+    --mount "type=bind,src=${project_dir}/chill-static-example/queries,dst=/home/chill/app/queries" \
+    --mount "type=bind,src=${project_dir}/chill-static-example/templates,dst=/home/chill/app/templates" \
+    -e CHILL_MEDIA_PATH="/media/" \
+    -e CHILL_THEME_STATIC_PATH="/theme/0/" \
+    -e CHILL_DESIGN_TOKENS_HOST="/design-tokens/0/" \
+    -e IMMUTABLE_EXAMPLE_URI="/immutable-example/v1/fake-hash/" \
+    "$slugname-chill-static-example"
+}
 
-docker image rm "$slugname-nginx" > /dev/null 2>&1 || printf ""
-DOCKER_BUILDKIT=1 docker build \
-    -t "$slugname-nginx" \
-    "${project_dir}/nginx"
-docker run -d \
-  -p "$app_port:$app_port" \
-  --name "$slugname-nginx" \
-  --network chillboxnet \
-  --mount "type=bind,src=${project_dir}/nginx/templates,dst=/build/templates" \
-  -e SLUGNAME="site1" \
-  -e VERSION="$site_version_string" \
-  -e IMMUTABLE_EXAMPLE_URI="/immutable-example/v1/fake-hash/" \
-  -e SERVER_NAME="localhost" \
-  -e SERVER_PORT="$app_port" \
-  -e IMMUTABLE_EXAMPLE="http://$slugname-immutable-example:8080/" \
-  -e IMMUTABLE_BUCKET_DOMAIN_NAME="http://chillbox-minio:9000" \
-  -e API="http://$slugname-api:8100" \
-  -e CHILL_STATIC_EXAMPLE="http://$slugname-chill-static-example:5000" \
-  -e CHILL_DYNAMIC_EXAMPLE="http://$slugname-chill-dynamic-example:5001" \
-  "$slugname-nginx"
+build_start_chill_dynamic_example() {
+  docker image rm "$slugname-chill-dynamic-example" > /dev/null 2>&1 || printf ""
+  DOCKER_BUILDKIT=1 docker build \
+      -t "$slugname-chill-dynamic-example" \
+      "${project_dir}/chill-dynamic-example"
+  docker run -d \
+    --name "$slugname-chill-dynamic-example" \
+    --network chillboxnet \
+    --mount "type=bind,src=${project_dir}/chill-dynamic-example/documents,dst=/home/chill/app/documents" \
+    --mount "type=bind,src=${project_dir}/chill-dynamic-example/queries,dst=/home/chill/app/queries" \
+    --mount "type=bind,src=${project_dir}/chill-dynamic-example/templates,dst=/home/chill/app/templates" \
+    -e CHILL_MEDIA_PATH="/media/" \
+    -e CHILL_THEME_STATIC_PATH="/theme/0/" \
+    -e CHILL_DESIGN_TOKENS_HOST="/design-tokens/0/" \
+    "$slugname-chill-dynamic-example"
+}
+
+build_start_nginx() {
+  docker image rm "$slugname-nginx" > /dev/null 2>&1 || printf ""
+  DOCKER_BUILDKIT=1 docker build \
+      -t "$slugname-nginx" \
+      "${project_dir}/nginx"
+  docker run -d \
+    -p "$app_port:$app_port" \
+    --name "$slugname-nginx" \
+    --network chillboxnet \
+    --mount "type=bind,src=${project_dir}/nginx/templates,dst=/build/templates" \
+    -e SLUGNAME="site1" \
+    -e VERSION="$site_version_string" \
+    -e IMMUTABLE_EXAMPLE_URI="/immutable-example/v1/fake-hash/" \
+    -e SERVER_NAME="localhost" \
+    -e SERVER_PORT="$app_port" \
+    -e IMMUTABLE_EXAMPLE="http://$slugname-immutable-example:8080/" \
+    -e IMMUTABLE_BUCKET_DOMAIN_NAME="http://chillbox-minio:9000" \
+    -e API="http://$slugname-api:8100" \
+    -e CHILL_STATIC_EXAMPLE="http://$slugname-chill-static-example:5000" \
+    -e CHILL_DYNAMIC_EXAMPLE="http://$slugname-chill-dynamic-example:5001" \
+    "$slugname-nginx"
+}
+
+build_start_immutable_example
+build_start_api
+build_start_chill_static_example
+build_start_chill_dynamic_example
+build_start_nginx
 
 sleep 2
 for container_name in \
