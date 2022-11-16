9171b15649886308d672f593bf9b0c21c848a80d
Jake Hickenlooper <jake@weboftomorrow.com>
2022-11-16 07:44:23 -0700

    wip

--- bin/local-start.sh
+++ bin/local-start.sh
@@ -12,9 +12,9 @@ script_dir="$(dirname "$(realpath "$0")")"
 project_dir="$(dirname "${script_dir}")"
 script_name="$(basename "$0")"
 site_version_string="$(make --silent -C "$project_dir" inspect.VERSION)"
+
 immutable_example_hash="$(make --silent -C "$project_dir/immutable-example/" inspect.HASH)"
 immutable_example_port=8080
-site_env_vars_file="$project_dir/.local-start-site-env-vars"
 
 project_dir_basename="$(basename "$project_dir")"
 project_name_hash="$(printf "%s" "$project_dir" | md5sum | cut -d' ' -f1)"
@@ -24,10 +24,18 @@ test "${#project_name_hash}" -eq "32" || (echo "ERROR $script_name: Failed to cr
 # depending on the project directory path at the time.
 # https://specifications.freedesktop.org/basedir-spec/basedir-spec-latest.html
 site_data_home="${XDG_DATA_HOME:-"$HOME/.local/share"}/$project_dir_basename-$slugname--$project_name_hash"
+# Store the generated env vars file in the application state dir since it needs
+# to persist after the script ends.
+site_state_home="${XDG_STATE_HOME:-"$HOME/.local/state"}/$project_dir_basename-$slugname--$project_name_hash"
+mkdir -p "$site_data_home"
+mkdir -p "$site_state_home"
 
 not_encrypted_secrets_dir="$site_data_home/not-encrypted-secrets"
+site_env_vars_file="$site_state_home/local-start-site-env-vars"
 
 cat <<MEOW > "$site_env_vars_file"
+# Generated from $0 on $(date)
+
 ARTIFACT_BUCKET_NAME=chillboxartifact
 AWS_PROFILE=chillbox_object_storage
 CHILLBOX_ARTIFACT=not-applicable
@@ -43,31 +51,32 @@ S3_ENDPOINT_URL=http://chillbox-minio:9000
 SERVER_PORT=$app_port
 SITES_ARTIFACT=not-applicable
 SLUGNAME=$slugname
-TECH_EMAIL=me@local.test
+TECH_EMAIL=llama@local.test
 VERSION=$site_version_string
 CHILL_STATIC_EXAMPLE_TRY_FILES_LAST_PARAM=@chill-static-example
 CHILL_STATIC_EXAMPLE_PATH=/
 CHILL_STATIC_EXAMPLE_PORT=5000
 CHILL_STATIC_EXAMPLE_SCHEME=http
-CHILL_STATIC_EXAMPLE_HOST=$slugname-chill-static-example
+CHILL_STATIC_EXAMPLE_HOST=$slugname-chill-static-example-$project_name_hash
 CHILL_DYNAMIC_EXAMPLE_PATH=/dynamic/
 CHILL_DYNAMIC_EXAMPLE_PORT=5001
 CHILL_DYNAMIC_EXAMPLE_SCHEME=http
-CHILL_DYNAMIC_EXAMPLE_HOST=$slugname-chill-dynamic-example
+CHILL_DYNAMIC_EXAMPLE_HOST=$slugname-chill-dynamic-example-$project_name_hash
 API_PATH=/api/
 API_PORT=8100
 API_SCHEME=http
-API_HOST=$slugname-api
+API_HOST=$slugname-api-$project_name_hash
 IMMUTABLE_EXAMPLE_HASH=$immutable_example_hash
 IMMUTABLE_EXAMPLE_PATH=/immutable-example/v1/$immutable_example_hash/
 IMMUTABLE_EXAMPLE_PORT=$immutable_example_port
-IMMUTABLE_EXAMPLE_URL=http://$slugname-immutable-example:$immutable_example_port/
+IMMUTABLE_EXAMPLE_HOST=$slugname-immutable-example-$project_name_hash
+IMMUTABLE_EXAMPLE_URL=http://$slugname-immutable-example-$project_name_hash:$immutable_example_port/
 MEOW
 . "$site_env_vars_file"
 
 . "$script_dir/utils.sh"
 
-stop_and_rm_containers_silently "$slugname"
+stop_and_rm_containers_silently "$slugname" "$project_name_hash" chill-dynamic-example api chill-static-example immutable-example nginx
 
 chillbox_minio_state="$(docker inspect --format '{{.State.Running}}' chillbox-minio || printf "false")"
 chillbox_local_shared_secrets_state="$(docker inspect --format '{{.State.Running}}' chillbox-local-shared-secrets || printf "false")"
@@ -81,80 +90,88 @@ fi
 # The ports on these do not need to be exposed since nginx is in front of them.
 
 build_start_immutable_example() {
-  docker image rm "$slugname-immutable-example" > /dev/null 2>&1 || printf ""
+  service_handler="immutable-example"
+  docker image rm "$slugname-$service_handler-$project_name_hash" > /dev/null 2>&1 || printf ""
   DOCKER_BUILDKIT=1 docker build \
       --target build \
-      -t "$slugname-immutable-example" \
-      "${project_dir}/immutable-example"
+      -t "$slugname-$service_handler-$project_name_hash" \
+      "$project_dir/$service_handler"
   docker run -d \
     --network chillboxnet \
     --env-file "$site_env_vars_file" \
-    --mount "type=bind,src=${project_dir}/immutable-example/src,dst=/build/src" \
-    --name "$slugname-immutable-example" "$slugname-immutable-example"
+    --mount "type=bind,src=$project_dir/$service_handler/src,dst=/build/src,readonly" \
+    --name "$slugname-$service_handler-$project_name_hash" \
+    "$slugname-$service_handler-$project_name_hash"
 }
 
 build_start_api() {
-  docker image rm "$slugname-api" > /dev/null 2>&1 || printf ""
+  service_handler="api"
+  docker image rm "$slugname-$service_handler-$project_name_hash" > /dev/null 2>&1 || printf ""
   DOCKER_BUILDKIT=1 docker build \
-    -t "$slugname-api" \
-    "${project_dir}/api"
+    -t "$slugname-$service_handler-$project_name_hash" \
+    "$project_dir/$service_handler"
   # Switch to root user when troubleshooting or using bind mounts
-  echo "Running the $slugname-api container with root user."
+  echo "Running the $slugname-$service_handler-$project_name_hash container with root user."
   docker run -d --tty \
-    --name "$slugname-api" \
+    --name "$slugname-$service_handler-$project_name_hash" \
     --user root \
     --env-file "$site_env_vars_file" \
     -e HOST="localhost" \
     -e PORT="$API_PORT" \
     -e SECRETS_CONFIG="/var/lib/local-secrets/site1/api/api-bridge.secrets.cfg" \
     --network chillboxnet \
-    --mount "type=bind,src=${project_dir}/api/src/site1_api,dst=/usr/local/src/app/src/site1_api,readonly" \
+    --mount "type=bind,src=$project_dir/api/src/site1_api,dst=/usr/local/src/app/src/site1_api,readonly" \
     --mount "type=bind,src=$not_encrypted_secrets_dir/api/api-bridge.secrets.cfg,dst=/var/lib/local-secrets/site1/api/api-bridge.secrets.cfg,readonly" \
-    $slugname-api ./flask-run.sh
+    "$slugname-$service_handler-$project_name_hash" ./flask-run.sh
 }
 
 build_start_chill_static_example() {
-  docker image rm "$slugname-chill-static-example" > /dev/null 2>&1 || printf ""
+  service_handler="chill-static-example"
+  docker image rm "$slugname-$service_handler-$project_name_hash" > /dev/null 2>&1 || printf ""
   DOCKER_BUILDKIT=1 docker build \
-      -t "$slugname-chill-static-example" \
-      "${project_dir}/chill-static-example"
+      -t "$slugname-$service_handler-$project_name_hash" \
+      "$project_dir/$service_handler"
   docker run -d \
-    --name "$slugname-chill-static-example" \
+    --name "$slugname-$service_handler-$project_name_hash" \
     --network chillboxnet \
     --env-file "$site_env_vars_file" \
-    --mount "type=bind,src=${project_dir}/chill-static-example/documents,dst=/home/chill/app/documents" \
-    --mount "type=bind,src=${project_dir}/chill-static-example/queries,dst=/home/chill/app/queries" \
-    --mount "type=bind,src=${project_dir}/chill-static-example/templates,dst=/home/chill/app/templates" \
-    "$slugname-chill-static-example"
+    --mount "type=volume,src=$slugname-$service_handler-$project_name_hash,dst=/var/lib/chill/sqlite3" \
+    --mount "type=bind,src=$project_dir/$service_handler/documents,dst=/home/chill/app/documents" \
+    --mount "type=bind,src=$project_dir/$service_handler/queries,dst=/home/chill/app/queries" \
+    --mount "type=bind,src=$project_dir/$service_handler/templates,dst=/home/chill/app/templates" \
+    "$slugname-$service_handler-$project_name_hash"
 }
 
 build_start_chill_dynamic_example() {
-  docker image rm "$slugname-chill-dynamic-example" > /dev/null 2>&1 || printf ""
+  service_handler="chill-dynamic-example"
+  docker image rm "$slugname-$service_handler-$project_name_hash" > /dev/null 2>&1 || printf ""
   DOCKER_BUILDKIT=1 docker build \
-      -t "$slugname-chill-dynamic-example" \
-      "${project_dir}/chill-dynamic-example"
+      -t "$slugname-$service_handler-$project_name_hash" \
+      "$project_dir/$service_handler"
   docker run -d \
-    --name "$slugname-chill-dynamic-example" \
+    --name "$slugname-$service_handler-$project_name_hash" \
     --network chillboxnet \
     --env-file "$site_env_vars_file" \
-    --mount "type=bind,src=${project_dir}/chill-dynamic-example/documents,dst=/home/chill/app/documents" \
-    --mount "type=bind,src=${project_dir}/chill-dynamic-example/queries,dst=/home/chill/app/queries" \
-    --mount "type=bind,src=${project_dir}/chill-dynamic-example/templates,dst=/home/chill/app/templates" \
-    "$slugname-chill-dynamic-example"
+    --mount "type=volume,src=$slugname-$service_handler-$project_name_hash,dst=/var/lib/chill/sqlite3" \
+    --mount "type=bind,src=$project_dir/$service_handler/documents,dst=/home/chill/app/documents" \
+    --mount "type=bind,src=$project_dir/$service_handler/queries,dst=/home/chill/app/queries" \
+    --mount "type=bind,src=$project_dir/$service_handler/templates,dst=/home/chill/app/templates" \
+    "$slugname-$service_handler-$project_name_hash"
 }
 
 build_start_nginx() {
-  docker image rm "$slugname-nginx" > /dev/null 2>&1 || printf ""
+  service_handler="nginx"
+  docker image rm "$slugname-$service_handler-$project_name_hash" > /dev/null 2>&1 || printf ""
   DOCKER_BUILDKIT=1 docker build \
-      -t "$slugname-nginx" \
-      "${project_dir}/nginx"
+      -t "$slugname-$service_handler-$project_name_hash" \
+      "$project_dir/$service_handler"
   docker run -d \
     -p "$app_port:$app_port" \
-    --name "$slugname-nginx" \
+    --name "$slugname-$service_handler-$project_name_hash" \
     --network chillboxnet \
     --env-file "$site_env_vars_file" \
-    --mount "type=bind,src=${project_dir}/nginx/templates,dst=/build/templates" \
-    "$slugname-nginx"
+    --mount "type=bind,src=$project_dir/$service_handler/templates,dst=/build/templates" \
+    "$slugname-$service_handler-$project_name_hash"
 }
 
 if [ ! -e "$not_encrypted_secrets_dir/api/api-bridge.secrets.cfg" ]; then
@@ -168,30 +185,9 @@ build_start_chill_dynamic_example
 build_start_nginx
 
 sleep 2
-for container_name in \
-  $slugname-chill-static-example \
-  $slugname-chill-dynamic-example \
-  $slugname-immutable-example \
-  $slugname-api \
-  $slugname-nginx \
-  ; do
-  echo ""
-  echo "### Logs for $container_name ###"
-  docker logs $container_name
-  echo ""
-  echo "### End logs for $container_name ###"
-  echo ""
-done
+output_all_logs_on_containers "$slugname" "$project_name_hash" chill-dynamic-example api chill-static-example immutable-example nginx
 
-for container_name in \
-  $slugname-chill-static-example \
-  $slugname-chill-dynamic-example \
-  $slugname-immutable-example \
-  $slugname-api \
-  $slugname-nginx \
-  ; do
-  echo "$container_name $(docker container inspect $container_name | jq '.[0].State.Status + .[0].State.Error')"
-done
+show_container_state "$slugname" "$project_name_hash" chill-dynamic-example api chill-static-example immutable-example nginx
 
 echo "The $slugname site is running on http://localhost:$app_port/ "
 
--- bin/utils.sh
+++ bin/utils.sh
@@ -1,22 +1,54 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 set -o errexit
 set -o nounset
 
 stop_and_rm_containers_silently () {
   slugname="$1"
+  shift 1
+  project_name_hash="$1"
+  shift 1
+  service_handler_names="$*"
+
   # A fresh start of the containers are needed. Hide any error output and such
   # from this as it is irrelevant.
-  docker stop --time 1 $slugname-chill-dynamic-example > /dev/null 2>&1 &
-  docker stop --time 1 $slugname-api > /dev/null 2>&1 &
-  docker stop --time 1 $slugname-chill-static-example > /dev/null 2>&1 &
-  docker stop --time 1 $slugname-immutable-example > /dev/null 2>&1 &
-  docker stop --time 1 $slugname-nginx > /dev/null 2>&1 &
+  for service_handler in $service_handler_names; do
+    docker stop --time 1 "$slugname-$service_handler-$project_name_hash" > /dev/null 2>&1 &
+  done
   wait
 
-  docker container rm $slugname-chill-dynamic-example > /dev/null 2>&1 || printf ''
-  docker container rm $slugname-api > /dev/null 2>&1 || printf ''
-  docker container rm $slugname-chill-static-example > /dev/null 2>&1 || printf ''
-  docker container rm $slugname-immutable-example > /dev/null 2>&1 || printf ''
-  docker container rm $slugname-nginx > /dev/null 2>&1 || printf ''
+  for service_handler in $service_handler_names; do
+    docker container rm "$slugname-$service_handler-$project_name_hash" > /dev/null 2>&1 || printf ''
+  done
+}
+
+output_all_logs_on_containers () {
+  slugname="$1"
+  shift 1
+  project_name_hash="$1"
+  shift 1
+  service_handler_names="$*"
+
+  for service_handler in $service_handler_names; do
+    container_name="$slugname-$service_handler-$project_name_hash"
+    echo ""
+    echo "### Logs for $container_name ###"
+    docker logs "$container_name"
+    echo ""
+    echo "### End logs for $container_name ###"
+    echo ""
+  done
+}
+
+show_container_state () {
+  slugname="$1"
+  shift 1
+  project_name_hash="$1"
+  shift 1
+  service_handler_names="$*"
+
+  for service_handler in $service_handler_names; do
+    container_name="$slugname-$service_handler-$project_name_hash"
+    echo "$container_name $(docker container inspect $container_name | jq '.[0].State.Status + .[0].State.Error')"
+  done
 }
